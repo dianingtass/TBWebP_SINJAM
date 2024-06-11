43bcccf841867504f651095abbde8e8385459c56
dianingtass <dianingtas@gmail.com>
2024-06-11 16:56:58 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'sinjam';
+
+    $conn = mysqli_connect($server, $username, $password, $database);
+?>
