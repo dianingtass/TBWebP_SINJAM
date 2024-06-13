fb73ad2ea783bd288a3fd5d3bdc920bacb8ad3e1
Septian Sugiarta <septiansugiarta3@gmail.com>
2024-06-13 18:15:52 +0700

    Update templateNavbarFooter.php

--- templateNavbarFooter.php
+++ templateNavbarFooter.php
@@ -1,7 +1,24 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Untree.co">
+  <link rel="shortcut icon" href="favicon.png">
+
+  <meta name="description" content="" />
+  <meta name="keywords" content="bootstrap, bootstrap4" />
+
+		<!-- Bootstrap CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+		<link href="css/tiny-slider.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
+	</head>
+
 <!-- TEMPLATE NAVBAR -->
         <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
-			<div class="container">
+			<div class="container fixed">
 				<a class="navbar-brand" href="index.html">SINJAM<span>UPNVJ</span></a>
 
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
@@ -96,3 +113,84 @@
 </style>
 
 <!-- TEMPLATE FOOTER -->
+<footer class="footer-section">
+			<div class="container relative">
+        <div class="row">
+          <div class="col-4 mt-5">
+            <a href="index.html" style="font-weight: 650; font-size: 32px; color:#208aae">SINJAM<span style="font-weight:100; color: black;">UPNVJ</span></a>
+          </div>
+          <div class="col-4">
+            <div class="mb-4 footer-h1">Menu</div>
+            <p class="mb-2"><a href="fasilitas.php">Fasilitas</a></p>
+            <p class="mb-2"><a href="cekKetersediaan.php">Jadwal</a></p>
+            <p class="mb-2"><a href="formPeminjaman.php">Peminjaman</a></p>
+            <p class="mb-2"><a href="feedback.php">Feedback</a></p>
+            <p class="mb-2"><a href="FAQ.php">FAQ</a></p>
+          </div>
+          <div class="col-4">
+            <div class="ml-9">
+              <div class="mb-4 footer-h1">Contact</div>
+              <ul class="list-unstyled">
+                <li>Universitas Pembangunan Nasional "Veteran" Jakarta</li>
+                <li>Jl. RS. Fatmawati, Pondok Labu, Jakarta Selatan, DKI Jakarta. 12450.</li>
+                <li>+6221-123 5678</li>
+                <li>sinjam@upnvj.ac.id</li>
+                
+              </ul>
+            </div>
+            <br><br>
+          </div>
+        
+				<div class="border-top copyright">
+					<div class="row mt-3">
+						<div class="col">
+							<p class="mb-2 text-center ">Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Sistem Informasi Peminjaman Universitas Pembangunan Nasional "Veteran" Jakarta <!-- License information: https://untree.co/license/ -->
+            </p>
+						</div>
+					</div>
+				</div>
+			</div>
+</footer>
+
+<style>
+  .footer-section {
+    padding: 80px 0;
+    background: #ffffff;
+    font-family: Gotham;
+  }
+
+  .footer-section .relative {
+    position: relative;
+  }
+
+  .footer-section a {
+    text-decoration: none;
+    color: #2f2f2f;
+    -webkit-transition: .3s all ease;
+    -o-transition: .3s all ease;
+    transition: .3s all ease; 
+  }
+
+  .footer-section a:hover {
+      color: #208aae; 
+    }
+
+  .footer-section .relative ul li {
+      margin-bottom: 10px; 
+  }
+
+  .footer-section .footer-h1 {
+    font-size: 20px;
+    font-weight: bold;
+    text-decoration: none;
+    color: black; }
+
+  .footer-section .border-top {
+    border-color: #dce5e4; 
+  }
+    
+  .footer-section .border-top.copyright {
+      font-size: 14px !important; }
+    </style>
+
+
